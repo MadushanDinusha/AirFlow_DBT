85b55f3908c6713aecd15eba8c37f0fbe938d1e4
Madushan <34265286+MadushanDinusha@users.noreply.github.com>
2024-06-15 23:08:59 +0530

    Update process_excel.py

--- dags/process_excel.py
+++ dags/process_excel.py
@@ -8,6 +8,8 @@ import json
 from xml.etree import ElementTree as ET
 import csv
 import re
+import shutil
+import os
 
 # Define your database connection details
 dbname = "dbt_poc"  # Update dbname
@@ -69,6 +71,8 @@ def extract_and_rebuild_xml(xml_file, metadata_table, output_csv):
         for xml_str, json_data in zip(all_xml_str, all_json_data):
             writer.writerow([xml_str, json_data])
 
+    move_files(source_directory, destination_directory, file_ext)
+
 def xml_to_dict(xml_str):
     root = ET.fromstring(xml_str)
     return {root.tag: xml_to_dict_helper(root)}
@@ -88,7 +92,25 @@ def xml_to_dict_helper(element):
             result[child.tag] = child_result
     return result
 
-# Define the default arguments for the DAG
+def move_files(source_dir, destination_dir, file_extension=None):
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+        print(f"Created destination directory {destination_dir}")
+
+    for filename in os.listdir(source_dir):
+        if file_extension is None or filename.endswith(file_extension):
+            source_file = os.path.join(source_dir, filename)
+            destination_file = os.path.join(destination_dir, filename)
+            shutil.move(source_file, destination_file)
+            print(f"Moved {source_file} to {destination_file}")
+
+# Example usage
+source_directory = '/usr/local/airflow/madhu'
+destination_directory = '/usr/local/airflow/madhu/archive'
+file_ext = '.xml'  
+
+
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -112,7 +134,7 @@ with DAG(
         task_id='file_sensor',
         fs_conn_id='file_system',  # This needs to be set up in the Airflow connections
         filepath='/usr/local/airflow/madhu/SMFsourcefile.xml',
-        poke_interval=5,  # Check for the file every 30 seconds
+        poke_interval=30,  # Check for the file every 30 seconds
         timeout=600  # Timeout after 10 minutes if the file is not found
     ) 
 
@@ -126,10 +148,29 @@ with DAG(
         }
     )
 
-    # dbt_seed = BashOperator(
-    #     task_id='dbt_seed',
-    #     bash_command='cd /opt/airflow/dbt && dbt seed'
-    # )
+    dbt_seed = BashOperator(
+        task_id='dbt_seed',
+        bash_command='cd /opt/airflow/dbt && dbt seed'
+    )
 
+    dbt_secutiry = BashOperator(
+        task_id='securities_tbl',
+        bash_command='cd /opt/airflow/dbt && dbt run --select securities_tbl'
+    )
 
-    file_sensor >> extract_xml_task 
+    hub_security = BashOperator(
+        task_id='hub_security',
+        bash_command='cd /opt/airflow/dbt && dbt run --select hub_security'
+    )
+
+    sat_security = BashOperator(
+        task_id='sat_security',
+        bash_command='cd /opt/airflow/dbt && dbt run --select sat_security'
+    )
+
+    link_security = BashOperator(
+        task_id='link_security',
+        bash_command='cd /opt/airflow/dbt && dbt run --select link_security'
+    )
+    file_sensor >> extract_xml_task >> dbt_seed >> dbt_secutiry >> hub_security >> sat_security \
+    >> link_security
